d450786eae49e3dd7cd7386d05e69ee616ea6f2d
mkluszczynski <mkluszczynski07@gmail.com>
2024-11-29 01:58:17 +0100

    Fix aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@lib": "/src/lib",
+      "@lib": path.resolve(__dirname, "lib"),
     },
   },
 });
